ae7c9dc54894caa52e5d4469d7eb2da34a28a9be
greggy <sudakovgrigory@gmail.com>
2024-05-27 22:47:42 +0700

    Finish saltstack

--- software_detection/detect.py
+++ software_detection/detect.py
@@ -6,7 +6,7 @@ from typing import Dict, Optional, List
 from jinja2 import Environment, FileSystemLoader
 
 # Set up command line argument parsing
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Detect installed software.')
 parser.add_argument('config_path', type=str, help='The path to the config.json file')
 args = parser.parse_args()
 
