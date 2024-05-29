3cb63ca048426ae89fd75fb36d3f20636fdc9c88
greggy <sudakovgrigory@gmail.com>
2024-05-29 13:48:51 +0700

    keycloak

--- launch.sh
+++ launch.sh
@@ -7,8 +7,17 @@ ANSIBLE_USER="ansible"
 ANSIBLE_PASSWORD="ansible"
 SSH_PORT=2222
 
+# Проверка наличия аргумента для плейбука
+if [ -z "$1" ]; then
+    echo "Пожалуйста, укажите путь к плейбуку."
+    echo "Использование: $0 /путь/к/playbook.yaml"
+    exit 1
+fi
+
+PLAYBOOK_PATH=$1
+
 # Линтер
-ansible-lint playbook.yaml
+ansible-lint ${PLAYBOOK_PATH}
 
 # Получение IP-адреса контейнера
 HOST_IP="127.0.0.1"
@@ -32,4 +41,4 @@ ansible_python_interpreter=/usr/bin/python3
 EOL
 
 # Запуск Ansible playbook
-ANSIBLE_SUDO_PASS=${ANSIBLE_PASSWORD} ansible-playbook -i inventory.ini ./playbook.yaml -v
+ANSIBLE_SUDO_PASS=${ANSIBLE_PASSWORD} ansible-playbook -i inventory.ini ${PLAYBOOK_PATH} -v
