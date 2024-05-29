e2be3f57ae7072681541cbafce8b0f4e0c575b9a
greggy <sudakovgrigory@gmail.com>
2024-05-29 20:43:26 +0700

    Strongswan stage 2

--- software_detection/detect.py
+++ software_detection/detect.py
@@ -14,14 +14,7 @@ args = parser.parse_args()
 with open(args.config_path, 'r') as config_file:
     config = json.load(config_file)
 
-manufacturer = config['manufacturer']
 application_name = config['name']
-maintainer = config['maintainer']
-
-package_fields_need_eq = {
-    'name': config['package'],
-    'maintainer': maintainer
-}
 soft_name = application_name
 
 version_res = [r'([\d.]+)-.+', r'([\d.]+)']
@@ -30,44 +23,67 @@ oval_vars_file = 'oval_vars.xml'
 
 # Main function to execute the script
 if __name__ == '__main__':
-    # Get raw package information
-    get_package_cmd = "dpkg-query -f '${Package};;${Status};;${Maintainer};;${Architecture};;${Source};;${Version}\n' -W"
-    stream = os.popen(get_package_cmd)
-    packages_raw = stream.read()
-
-    # Parse package list
-    package_parsing_re = r'(?P<name>.+);;(?P<status>.+);;(?P<maintainer>.+);;(?P<arch>.+);;(?P<source>.*);;(?P<version>.+)'
-    r = re.compile(package_parsing_re)
-    packages = [i.groupdict() for i in r.finditer(packages_raw)]
-
-    # Filter packages based on fields
-    comp_keys = package_fields_need_eq.keys()
-    package = None
-    for p in packages:
-        res = True
-        for key in comp_keys:
-            if package_fields_need_eq.get(key) != p.get(key):
-                res = False
-                break
-        if res:
-            package = p
-            break
-
     # Extract version using multiple regexes
     context = {
         'name': None,
         'version': None
     }
-    if package:
-        package_version = package.get('version', '')
-        for version_re in version_res:
-            result = re.search(version_re, package_version)
-            if result:
-                context['version'] = result.groups()[0]
+
+    # Check for the "detect" property in the configuration
+    if 'detect' in config:
+        detect_command = config['detect'].get('command')
+        if detect_command:
+            stream = os.popen(detect_command)
+            command_output = stream.read()
+
+            # Extracting name and version from the command output
+            for version_re in version_res:
+                result = re.search(version_re, command_output)
+                if result:
+                    context['version'] = result.groups()[0]
+                    break
+
+            if context['version']:
+                context['name'] = soft_name
+    else:
+        package_fields_need_eq = {
+            'name': config['package'],
+            'maintainer': config['maintainer']
+        }
+        # Get raw package information
+        get_package_cmd = "dpkg-query -f '${Package};;${Status};;${Maintainer};;${Architecture};;${Source};;${Version}\n' -W"
+        stream = os.popen(get_package_cmd)
+        packages_raw = stream.read()
+
+        # Parse package list
+        package_parsing_re = r'(?P<name>.+);;(?P<status>.+);;(?P<maintainer>.+);;(?P<arch>.+);;(?P<source>.*);;(?P<version>.+)'
+        r = re.compile(package_parsing_re)
+        packages = [i.groupdict() for i in r.finditer(packages_raw)]
+
+        # Filter packages based on fields
+        comp_keys = package_fields_need_eq.keys()
+        package = None
+        for p in packages:
+            res = True
+            for key in comp_keys:
+                if package_fields_need_eq.get(key) != p.get(key):
+                    res = False
+                    break
+            if res:
+                package = p
                 break
 
-    if context['version']:
-        context['name'] = soft_name
+
+        if package:
+            package_version = package.get('version', '')
+            for version_re in version_res:
+                result = re.search(version_re, package_version)
+                if result:
+                    context['version'] = result.groups()[0]
+                    break
+
+        if context['version']:
+            context['name'] = soft_name
 
     # Render Jinja2 template to file with context
     template_dir = os.path.dirname(oval_vars_template_file)
