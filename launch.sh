41cbc032c14f0b68db0ec17efbfed2781fc4f351
greggy <sudakovgrigory@gmail.com>
2024-05-30 20:30:12 +0700

    launch.sh: --lint

--- launch.sh
+++ launch.sh
@@ -10,13 +10,20 @@ SSH_PORT=2222
 # Проверка наличия аргумента для плейбука
 if [ -z "$1" ]; then
     echo "Пожалуйста, укажите путь к плейбуку."
-    echo "Использование: $0 /путь/к/playbook.yaml"
+    echo "Использование: $0 /путь/к/playbook.yaml [--lint]"
     exit 1
 fi
 
 PLAYBOOK_PATH=$1
 
 export ANSIBLE_CONFIG="./ansible.cfg"
+
+# Проверка наличия параметра --lint
+if [ "$2" == "--lint" ]; then
+    # Линтер
+    ansible-lint ${PLAYBOOK_PATH}
+    exit 0
+fi
 
 # Линтер
 ansible-lint ${PLAYBOOK_PATH}
@@ -36,32 +43,3 @@ EOL
 
 # Запуск Ansible playbook
 ANSIBLE_SUDO_PASS=${ANSIBLE_PASSWORD} ansible-playbook -i inventory.ini ${PLAYBOOK_PATH} -vvv
-
-
-#  umask 77 && mkdir -p \"` echo /home/ansible/.ansible/tmp `\"&& mkdir \"` echo /home/ansible/.ansible/tmp/ansible-tmp-1716973304.9806573-4129973-70004313624527 `\" && echo ansible-tmp-1716973304.9806573-4129973-70004313624527=\"` echo /home/ansible/.ansible/tmp/ansible-tmp-1716973304.9806573-4129973-70004313624527 `\" 
-
-
-# # mkdir: cannot create directory '"/home/ansible/.ansible/tmp/ansible-tmp-1716973525.9867811-4136643-126860085612014"': No such file or directory
-
-
-## ours
-# umask 77 \
-#   && mkdir -p \"` echo /home/ansible/.ansible/tmp `\" \
-#   && mkdir /home/ansible/.ansible/tmp/ansible-tmp-1716973525.9867811-4136643-126860085612014 \
-#   && echo ansible-tmp-1716973525.9867811-4136643-126860085612014=\"` echo /home/ansible/.ansible/tmp/ansible-tmp-1716973525.9867811-4136643-126860085612014 `\"
-
-## from stackexchange
-# umask 77 \
-#   && mkdir -p \"` echo /tmp `\"\
-#   && mkdir /tmp/ansible-tmp-1626262883.727861-210-80403864334304 \
-#   && echo ansible-tmp-1626262883.727861-210-80403864334304=\"` echo /tmp/ansible-tmp-1626262883.727861-210-80403864334304 `\"
-
-
-## ours
-# umask 77 \
-#   && mkdir -p \"` echo /tmp/ `\" \
-#   && mkdir \"` echo /tmp/ansible-tmp-1716975908.4795759-13523-256169111411956 `\" \
-#   && echo ansible-tmp-1716975908.4795759-13523-256169111411956=\"` echo /tmp/ansible-tmp-1716975908.4795759-13523-256169111411956 `\" 
-
-# ssh -C -o ControlMaster=auto -o ControlPersist=60s -o Port=2222 -o 'User="ansible"' -o ConnectTimeout=10 -o StrictHostKeyChecking=no -o 'ControlPath="/home/griggy/.ansible/cp/1867d46516"' 127.0.0.1  '/bin/sh -c mkdir /tmp/test' 
-# ssh -t -C -o ControlMaster=auto -o ControlPersist=60s -o Port=2222 -o 'User="ansible"' -o ConnectTimeout=10 -o StrictHostKeyChecking=no -o 'ControlPath="/home/griggy/.ansible/cp/1867d46516"' 127.0.0.1 /bin/sh
