54f1637abafc1eed1a9a76650776a9321f807582
greggy <sudakovgrigory@gmail.com>
2024-05-30 21:50:55 +0700

    Update CLEANUP.sh

--- CLEANUP.sh
+++ CLEANUP.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rm /etc/apt/apt.conf.d/99allow-insecure
-rm /etc/apt/apt.conf.d/99disable-ssl-verification
+sudo rm /etc/apt/apt.conf.d/99allow-insecure /etc/apt/apt.conf.d/99disable-ssl-verification
