9212f89b2c07636d91bab5e9c4b5de1daf318558
greggy <sudakovgrigory@gmail.com>
2024-05-30 20:25:48 +0700

    CouchDB (not working)

--- new.sh
+++ new.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir -p nginx_unit_from_official_package_on_debian/roles/nginx_unit_from_official_package_on_debian/{defaults,tasks,handlers,files,templates,vars}
+mkdir -p couchdb_from_official_on_debian/roles/couchdb_from_official_on_debian/{defaults,tasks,handlers,files,templates,vars}
